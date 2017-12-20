ec2f01ec71e8601b083a7bb7a67269cc12e0169d
tbeck10 <trbeck10@gmail.com>
2017-12-20 16:37:06 -0500

    Add javascript to hide navbar when scrolling

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,8 +4,22 @@ $(document).ready(function(){
       SlidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 4000,
+      arrows: false,
       pauseOnHover: false,
       draggable: false,
       pauseOnFocus: false,
+  });
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
   });
 });
