3649ff76642eecd779233bec5c12a4bc3d45e361
tbeck10 <trbeck10@gmail.com>
2017-12-20 16:14:30 -0500

    Add carousel images, text, and functionality

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+      slidesToShow: 1,
+      SlidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnHover: false,
+      draggable: false,
+      pauseOnFocus: false,
+  });
+});
